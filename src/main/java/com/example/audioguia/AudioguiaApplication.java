2fecac45f859192427254ec857d6e5ed4d095203
MrBimPo <andresfloresgarcia914@gmail.com>
2024-04-09 10:16:15 +0200

    Initial commit

---
+++ src/main/java/com/example/audioguia/AudioguiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.audioguia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioguiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioguiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/audioguia/AudioguiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.audioguia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioguiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
